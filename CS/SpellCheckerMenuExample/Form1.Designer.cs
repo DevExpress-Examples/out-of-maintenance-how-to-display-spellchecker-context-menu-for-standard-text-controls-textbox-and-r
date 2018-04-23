00bee6dacb8d43384b2a334dc5cdb85c780292ce
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SpellCheckerMenuExample/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SpellCheckerMenuExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraSpellChecker.OptionsSpelling optionsSpelling1 = new DevExpress.XtraSpellChecker.OptionsSpelling();
+            DevExpress.XtraSpellChecker.OptionsSpelling optionsSpelling2 = new DevExpress.XtraSpellChecker.OptionsSpelling();
+            DevExpress.XtraSpellChecker.OptionsSpelling optionsSpelling3 = new DevExpress.XtraSpellChecker.OptionsSpelling();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.spellChecker1 = new DevExpress.XtraSpellChecker.SpellChecker(this.components);
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 81);
+            this.spellChecker1.SetSpellCheckerOptions(this.textBox1, optionsSpelling1);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Standard TextBox Control\r\n\r\nthe the errror dedxs";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(277, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(290, 326);
+            this.spellChecker1.SetSpellCheckerOptions(this.richTextBox1, optionsSpelling2);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Standard RichTextBox Control\n\nthe the errror";
+            // 
+            // spellChecker1
+            // 
+            this.spellChecker1.Culture = new System.Globalization.CultureInfo("en-US");
+            this.spellChecker1.ParentContainer = null;
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.EditValue = "DevExpress MemoEdit Control\r\n\r\nthe the errror";
+            this.memoEdit1.Location = new System.Drawing.Point(12, 109);
+            this.memoEdit1.Name = "memoEdit1";
+            this.spellChecker1.SetShowSpellCheckMenu(this.memoEdit1, true);
+            this.memoEdit1.Size = new System.Drawing.Size(231, 229);
+            this.spellChecker1.SetSpellCheckerOptions(this.memoEdit1, optionsSpelling3);
+            this.memoEdit1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 352);
+            this.Controls.Add(this.memoEdit1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private DevExpress.XtraSpellChecker.SpellChecker spellChecker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+    }
+}
+
